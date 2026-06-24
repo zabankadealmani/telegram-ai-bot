c207b84c23cff8dfcaf9263d0e8ae7625616d92d
zabankadealmani <zabankade.almani@gmail.com>
2026-06-24 22:20:56 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,18 @@
 import os
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 ربات فعاله")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
